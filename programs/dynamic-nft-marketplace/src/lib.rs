c45d2580025a289f8a79435c422d274406441245
Sushant Kumar <sushantkumar23@gmail.com>
2022-03-08 00:08:12 +0530

    Adding the contracts

--- programs/dynamic-nft-marketplace/src/lib.rs
+++ programs/dynamic-nft-marketplace/src/lib.rs
@@ -3,12 +3,31 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod dynamic_nft_marketplace {
+pub mod nft_marketplace {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize_nft(ctx: Context<InitializeNFT>, nft_address: Pubkey) -> Result<()> {
+        let nft_account = &mut ctx.accounts.nft_account;
+        nft_account.authority = *ctx.accounts.user.key;
+        nft_account.sold = false;
+        nft_account.nft_address = nft_address;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeNFT<'info> {
+    #[account(init, payer = user)]
+    pub nft_account: Account<'info, NFTAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct NFTAccount {
+    pub authority: Pubkey,
+    pub sold: bool,
+    pub nft_address: Pubkey,
+}
